df80c561d459d9e19554d07454d9e245611e3c0b
LG4L <alanlaurent2727@gmail.com>
2024-11-02 02:40:51 +0100

    CLI addition
    still not working ;(

--- babel/main.cpp
+++ babel/main.cpp
@@ -1,23 +1,54 @@
 #include "Library.h"
 #include "User.h"
-
+#include "Book.h"
 #include <iostream>
 
-int main() {
-    
-    Library::getInstance().someOperation();
-
-    User user("matthieu", "80 rue des penis", "095404949", "U123");
-
-    std::cout << "Name: " << user.getName() << std::endl;
-    std::cout << "Address: " << user.getAddr() << std::endl;
-    std::cout << "Phone: " << user.getPhone() << std::endl;
-    std::cout << "User ID: " << user.getId() << std::endl;
-
-    return 0;
+void interface()
+{
+	int choix;
+	std::cout << "Bienvenue dans la bibliothèque digital" << std::endl;
+	std::cout << "1. Ajouter un livre" << std::endl;
+	std::cout << "2. Supprimer un livre" << std::endl;
+	std::cout << "3. Rechercher un livre" << std::endl;
+	std::cout << "4. Emprunter un livre" << std::endl;
+	std::cout << "5. Retourner un livre" << std::endl;
+	std::cout << "6. Quitter" << std::endl;
+	std::cin >> choix;
+	
+	switch (choix)
+    {
+	case 1:
+	    std::cout << "Ajouter un livre selectionner" << std::endl;
+		std::string title;
+		std::cin >> title;
+		std::cout << "Quel et l'autheur de ce titre?" << std::endl;
+		std::string author;
+		std::cin >> author;
+		std::cout << "Le genre ?" << std::endl;
+        std::string gender;
+		std::cin >> gender;
+		std::cout << "la date de sortie" << std::endl;
+		int issuedate;
+		std::cin >> issuedate;
+		// la faut un "if" savoir si il existe déjà si il existe déjà faut faire +1 sinon l'ajouté
+		
+		Library::getInstance().addBook(title, author, gender, issuedate);
+	    break;
+	case 2:
+	    std::cout << "Ajouter un livre selectionner" << std::endl;
+	    break;
+	case 3:
+	    std::cout << "Ajouter un livre selectionner" << std::endl;
+	    break;
+	case 4:
+	    std::cout << "Ajouter un livre selectionner" << std::endl;
+	    break;
+	case 5:
+	    std::cout << "Ajouter un livre selectionner" << std::endl;
+	    break;
+	case 6:
+	    std::cout << "Ajouter un livre selectionner" << std::endl;
+	    break;
+	}
+	
 }
-// std::string Book::toString() {
-//    return "Titre: " + title + ",
-//    Auteur: " + author + ", Genre: " + gender + ",
-//    Publication: " + std::to_string(issueDate) + ",
-//    Exemplaires: " + std::to_string(examplary);
